483bc0a97f90ff4ea24fed0b97b6a780a771dc87
Sakari Ailus <sakari.ailus@linux.intel.com>
2018-07-03 17:19:27 -0400

    UPSTREAM: BACKPORT: media: v4l: mediabus: Recognise CSI-2 D-PHY and C-PHY
    The CSI-2 bus may use either D-PHY or C-PHY. Make this visible in media
    bus enum.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Tested-by: Steve Longerbeam <steve_longerbeam@mentor.com>
    Tested-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    
    (cherry picked from 2d95e7ed07ed29715a801a3d33b2ad2a6fb26ee3)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 140678708
    Change-Id: I881e044f788f9af9a5ca7f32ad048644916e279f

--- drivers/media/platform/stm32/stm32-dcmi.c
+++ drivers/media/platform/stm32/stm32-dcmi.c
@@ -1669,7 +1669,7 @@ static int dcmi_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	if (ep.bus_type == V4L2_MBUS_CSI2) {
+	if (ep.bus_type == V4L2_MBUS_CSI2_DPHY) {
 		dev_err(&pdev->dev, "CSI bus not supported\n");
 		return -ENODEV;
 	}
