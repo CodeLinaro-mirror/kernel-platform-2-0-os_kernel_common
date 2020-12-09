c7a85a37986909755434912984930f87fc6781a0
John Stultz <john.stultz@linaro.org>
2020-12-09 04:53:23 +0000

    RFC: dma-buf: heaps: Add deferred-free-helper code
    This patch provides infrastructure for deferring buffer frees.
    
    This is a feature ION provided which when used with some form
    of a page pool, provides a nice performance boost. The reason
    it helps is it allows the page-zeroing to be done out of the
    normal allocation/free path, and pushed off to a kthread.
    
    As not all heaps will find this useful, its implemented as
    a optional helper library that heaps can utilize.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: Ibdc6a6f44a6d7583817bdc808f8d4fb836b0210f

---
+++ drivers/dma-buf/heaps/deferred-free-helper.h
@@ -0,0 +1,12 @@
+#ifndef DEFERRED_FREE_HELPER_H
+#define DEFERRED_FREE_HELPER_H
+
+struct deferred_freelist_item {
+        size_t size;
+        void (*free)(struct deferred_freelist_item*, bool no_pool);
+        struct list_head list;
+};
+
+void deferred_free(struct deferred_freelist_item* item, void (*free)(struct deferred_freelist_item*, bool no_pool), size_t size);
+
+#endif
