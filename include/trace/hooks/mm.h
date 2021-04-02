623bcce2da516b4588cc0eccdd33f63476fb09d9
Minchan Kim <minchan@google.com>
2021-04-01 18:25:57 -0700

    ANDROID: mm: cma: add vendor hoook in cma_alloc()
    Add vendor hook for cma_alloc latency measuring.
    
    Bug: 177231781
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: Ia2dbb26454bd8f03489389b29b9a6c939d3c2bbb
    Signed-off-by: Richard Chang <richardycc@google.com>
    (cherry picked from commit c6e85ea56baac84818fd5d82dda064ef80eef2f6)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -12,6 +12,7 @@
 struct shmem_inode_info;
 struct folio;
 struct page_vma_mapped_walk;
+struct cma;
 
 DECLARE_RESTRICTED_HOOK(android_rvh_shmem_get_folio,
 			TP_PROTO(struct shmem_inode_info *info, struct folio **folio),
@@ -46,6 +47,13 @@ DECLARE_HOOK(android_vh_page_add_new_anon_rmap,
 	TP_PROTO(struct page *page, struct vm_area_struct *vma,
 		unsigned long address),
 	TP_ARGS(page, vma, address));
+DECLARE_HOOK(android_vh_cma_alloc_start,
+	TP_PROTO(s64 *ts),
+	TP_ARGS(ts));
+DECLARE_HOOK(android_vh_cma_alloc_finish,
+	TP_PROTO(struct cma *cma, struct page *page, unsigned long count,
+		 unsigned int align, gfp_t gfp_mask, s64 ts),
+	TP_ARGS(cma, page, count, align, gfp_mask, ts));
 DECLARE_HOOK(android_vh_meminfo_proc_show,
 	TP_PROTO(struct seq_file *m),
 	TP_ARGS(m));
