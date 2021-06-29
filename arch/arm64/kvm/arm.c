5e4de2b2f5b005b80d181ae3395d6e1750f474b9
David Brazdil <dbrazdil@google.com>
2021-06-29 22:05:26 +0000

    ANDROID: KVM: arm64: Introduce IOMMU driver infrastructure
    Bootstrap infrastructure for IOMMU drivers by introducing kvm_iommu_ops
    struct in EL2 that is populated based on a iommu_driver parameter to
    __pkvm_init hypercall and selected in EL1 early init.
    
    An 'init' operation is called in __pkvm_init_finalise, giving the driver
    an opportunity to initialize itself in EL2 and create any EL2 mappings
    that it will need. 'init' is specifically called before
    'finalize_host_mappings' so that:
      (a) pages mapped by the driver change owner to hyp,
      (b) ownership changes in 'finalize_host_mappings' get reflected in
          IOMMU mappings (added in a future patch).
    
    Test: builds, boots
    Bug: 190463801
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Change-Id: Icd496d193c0bbf811406c3a8d4b94610e2a03197

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1705,6 +1705,11 @@ static bool init_psci_relay(void)
 	return true;
 }
 
+static int init_stage2_iommu(void)
+{
+	return KVM_IOMMU_DRIVER_NONE;
+}
+
 static int init_subsystems(void)
 {
 	int err = 0;
@@ -1764,7 +1769,7 @@ static void teardown_hyp_mode(void)
 	}
 }
 
-static int do_pkvm_init(u32 hyp_va_bits)
+static int do_pkvm_init(u32 hyp_va_bits, enum kvm_iommu_driver iommu_driver)
 {
 	void *per_cpu_base = kvm_ksym_ref(kvm_arm_hyp_percpu_base);
 	int ret;
@@ -1773,7 +1778,7 @@ static int do_pkvm_init(u32 hyp_va_bits)
 	hyp_install_host_vector();
 	ret = kvm_call_hyp_nvhe(__pkvm_init, hyp_mem_base, hyp_mem_size,
 				num_possible_cpus(), kern_hyp_va(per_cpu_base),
-				hyp_va_bits);
+				hyp_va_bits, iommu_driver);
 	preempt_enable();
 
 	return ret;
@@ -1791,7 +1796,11 @@ static int kvm_hyp_init_protection(u32 hyp_va_bits)
 	if (ret)
 		return ret;
 
-	ret = do_pkvm_init(hyp_va_bits);
+	ret = init_stage2_iommu();
+	if (ret < 0)
+		return ret;
+
+	ret = do_pkvm_init(hyp_va_bits, (enum kvm_iommu_driver)ret);
 	if (ret)
 		return ret;
 
