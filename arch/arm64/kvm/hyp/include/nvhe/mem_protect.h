3dc190c0e900da66edd08efc66d3af4f09437759
David Brazdil <dbrazdil@google.com>
2021-10-28 14:26:18 +0100

    ANDROID: KVM: arm64: Add 'host_mmio_dabt_handler' to kvm_iommu_ops
    Add a new kvm_iommu_ops hook which allows the IOMMU driver to handle
    data aborts in unmapped device memory regions. If the abort is handled
    by the driver, the global abort handler will not attempt to map in the
    page.
    
    For example, this enables the IOMMU driver to virtualize access to
    the underlying IOMMU hardware, or to allow access to a subset of the
    functionality, eg. performance counters.
    
    Test: builds, boots
    Bug: 190463801
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Change-Id: Ie2e38652f0568c27d30190fcc3879592872863ae

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -55,7 +55,7 @@ bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id);
 int kvm_host_prepare_stage2(void *pgt_pool_base);
-void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt);
+void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt, bool is_dabt);
 
 static __always_inline void __load_host_stage2(void)
 {
