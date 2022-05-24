9cc9749b314def8e3220edd2f00d80c3566ebbf1
Daniel Rosenberg <drosen@google.com>
2022-05-24 13:15:25 -0700

    ANDROID: fuse-bpf: Add support for FUSE_COPY_FILE_RANGE
    Bug: 202785178
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I7dca23072ce9042a9af55076c75fffa657884388

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1535,6 +1535,30 @@ int fuse_lseek_initialize_out(struct bpf_fuse_args *fa, struct fuse_lseek_io *fl
 int fuse_lseek_backing(struct bpf_fuse_args *fa, struct file *file, loff_t offset, int whence);
 void *fuse_lseek_finalize(struct bpf_fuse_args *fa, struct file *file, loff_t offset, int whence);
 
+struct fuse_copy_file_range_io {
+	struct fuse_copy_file_range_in fci;
+	struct fuse_write_out fwo;
+};
+
+int fuse_copy_file_range_initialize_in(struct bpf_fuse_args *fa,
+				       struct fuse_copy_file_range_io *fcf,
+				       struct file *file_in, loff_t pos_in,
+				       struct file *file_out, loff_t pos_out,
+				       size_t len, unsigned int flags);
+int fuse_copy_file_range_initialize_out(struct bpf_fuse_args *fa,
+					struct fuse_copy_file_range_io *fcf,
+					struct file *file_in, loff_t pos_in,
+					struct file *file_out, loff_t pos_out,
+					size_t len, unsigned int flags);
+int fuse_copy_file_range_backing(struct bpf_fuse_args *fa,
+				 struct file *file_in, loff_t pos_in,
+				 struct file *file_out, loff_t pos_out,
+				 size_t len, unsigned int flags);
+void *fuse_copy_file_range_finalize(struct bpf_fuse_args *fa,
+				    struct file *file_in, loff_t pos_in,
+				    struct file *file_out, loff_t pos_out,
+				    size_t len, unsigned int flags);
+
 int fuse_fsync_initialize_in(struct bpf_fuse_args *fa, struct fuse_fsync_in *ffi,
 			     struct file *file, loff_t start, loff_t end, int datasync);
 int fuse_fsync_initialize_out(struct bpf_fuse_args *fa, struct fuse_fsync_in *ffi,
