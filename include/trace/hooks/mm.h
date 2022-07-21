f55edcc1c1db63ec20c98074cfb0da9174a5bb26
liuhailong <liuhailong@oppo.com>
2022-07-21 10:08:14 +0800

    ANDROID: vendor_hooks: add hooks in __alloc_pages_slowpath
    Since android_vh_alloc_pages_slowpath is revert by
    commit e09000ee1915 ("Revert half of "ANDROID: vendor_hooks: Add hooks
    for memory when debug""). re-add hooks here to measure the duration
    
    Bug: 182443489
    Bug: 244486242
    Signed-off-by: liuhailong <liuhailong@oppo.com>
    Change-Id: Ie4534047105d8409623692cc3811b55d9ddbd17d
    (cherry picked from commit 4536de1b708cd4cccf9d47ba8e368b2df67ba10c)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -68,7 +68,16 @@ DECLARE_HOOK(android_vh_include_reserved_zone,
 DECLARE_HOOK(android_vh_show_mem,
 	TP_PROTO(unsigned int filter, nodemask_t *nodemask),
 	TP_ARGS(filter, nodemask));
+<<<<<<< HEAD   (69899f FROMGIT: arm64: fix oops in concurrently setting insn_emulat)
 struct slabinfo;
+=======
+DECLARE_HOOK(android_vh_alloc_pages_slowpath_begin,
+	     TP_PROTO(gfp_t gfp_mask, unsigned int order, unsigned long *pdata),
+	     TP_ARGS(gfp_mask, order, pdata));
+DECLARE_HOOK(android_vh_alloc_pages_slowpath_end,
+	     TP_PROTO(gfp_t gfp_mask, unsigned int order, unsigned long data),
+	     TP_ARGS(gfp_mask, order, data));
+>>>>>>> CHANGE (4536de ANDROID: vendor_hooks: add hooks in __alloc_pages_slowpath)
 struct dirty_throttle_control;
 DECLARE_HOOK(android_vh_mm_dirty_limits,
 	TP_PROTO(struct dirty_throttle_control *const gdtc, bool strictlimit,
