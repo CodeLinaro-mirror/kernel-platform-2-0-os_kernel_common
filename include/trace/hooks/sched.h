d2bf44f7357287b3f13907481b4cd19c8d4e5efb
Khalid Shaik <khalid.s@samsung.com>
2022-11-23 21:33:25 +0530

    ANDROID: Sched: Add restricted vendor hook in schedule()
    This vendor hook is used to override the scheduler decisions
    with our vendor scheduler logic
    
    This vendor hook was part of
    commit 292f430816f36384634ca1365ccc67c08302dd21
    (ANDROID: Sched: Add restricted vendor hooks for scheduler)
    
    But commit 7219ca326a8e84df0c88db8aa4eafd793a55527c
    (Revert "ANDROID: Sched: Add restricted vendor hooks for scheduler")
    deleted above vendor hooks stating unused
    
    Adding back above vendor hooks required for EMS.
    
    Bug: 259900798
    
    Change-Id: I1d8c7b70223d85c10127db3794c2ef242b0abb3e
    Signed-off-by: Khalid Shaik <khalid.s@samsung.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -157,6 +157,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_tick_entry,
 	TP_PROTO(struct rq *rq),
 	TP_ARGS(rq), 1);
 
+DECLARE_RESTRICTED_HOOK(android_rvh_schedule,
+	TP_PROTO(struct task_struct *prev, struct task_struct *next, struct rq *rq),
+	TP_ARGS(prev, next, rq), 1);
+
 DECLARE_RESTRICTED_HOOK(android_rvh_build_perf_domains,
 	TP_PROTO(bool *eas_check),
 	TP_ARGS(eas_check), 1);
