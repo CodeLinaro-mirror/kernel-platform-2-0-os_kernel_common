b9b68fa416496246ed12d4d425628e9bab96d2c7
Sebastian Ene <sebastianene@google.com>
2023-02-10 15:59:10 +0000

    ANDROID: KVM: arm64: Add smc64 trap handling for protected guests
    The guest FF-A will be using smc64 convention which has to be handled on
    a separate path. Add support for trapping these instructions for
    protected guests only. Forward these request to the secure OS and add a
    mockup function that will do the filtering of these calls based on an
    allow list.
    
    Change-Id: I0207608c878153e5acb70d1b5e0ceb917248da43
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/ffa.h
+++ arch/arm64/kvm/hyp/include/nvhe/ffa.h
@@ -7,12 +7,14 @@
 #define __KVM_HYP_FFA_H
 
 #include <asm/kvm_host.h>
+#include <nvhe/pkvm.h>
 
 #define FFA_MIN_FUNC_NUM 0x60
 #define FFA_MAX_FUNC_NUM 0x7F
 
 int hyp_ffa_init(void *pages);
 bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt);
+bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu);
 
 static inline bool is_ffa_call(u64 func_id)
 {
