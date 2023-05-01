bcb9647f84f026527cc9a38f9ed750cec12f238b
Mostafa Saleh <smostafa@google.com>
2023-05-01 15:17:09 +0000

    ANDROID: KVM: arm64: iommu: Register IOMMU driver dynamically
    As step to modularize the driver, make IOMMU registration dynamic.
    Remove static calls to the SMMUv3 driver, and remove the IOMMU driver
    from pkvm_init HVC.
    
    Add a new file for IOMMU EL1 functions.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: Ie742f12c2307248f77e85af9480c4745e188ebb9
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -331,18 +331,6 @@ static int unmap_protected_regions(void)
 	return 0;
 }
 
-static int select_iommu_ops(enum kvm_iommu_driver driver)
-{
-	switch (driver) {
-	case KVM_IOMMU_DRIVER_NONE:
-		return 0;
-	case KVM_IOMMU_DRIVER_SMMUV3:
-		return kvm_arm_smmu_v3_register();
-	}
-
-	return -EINVAL;
-}
-
 void __noreturn __pkvm_init_finalise(void)
 {
 	struct kvm_host_data *host_data = this_cpu_ptr(&kvm_host_data);
@@ -384,8 +372,8 @@ void __noreturn __pkvm_init_finalise(void)
 	if (ret)
 		goto out;
 
-	if (kvm_iommu_ops.init) {
-		ret = kvm_iommu_ops.init();
+	if (kvm_iommu_ops->init) {
+		ret = kvm_iommu_ops->init();
 		if (ret)
 			goto out;
 	}
@@ -418,8 +406,7 @@ out:
 }
 
 int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
-		unsigned long *per_cpu_base, u32 hyp_va_bits,
-		enum kvm_iommu_driver iommu_driver)
+		unsigned long *per_cpu_base, u32 hyp_va_bits)
 {
 	struct kvm_nvhe_init_params *params;
 	void *virt = hyp_phys_to_virt(phys);
@@ -446,9 +433,9 @@ int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
 	if (ret)
 		return ret;
 
-	ret = select_iommu_ops(iommu_driver);
-	if (ret)
-		return ret;
+	kvm_iommu_ops = kern_hyp_va(kvm_iommu_ops);
+	if (!kvm_iommu_ops)
+		return -EINVAL;
 
 	update_nvhe_init_params();
 
