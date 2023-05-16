13948df9d0345ef025d28db4acc52d876cb2b1ff
Alice Ryhl <aliceryhl@google.com>
2023-05-16 12:43:20 +0000

    ANDROID: rust_binder: define a Rust binder driver
    This defines the Rust binder driver, sets up the helpers for making C
    types accessible from Rust, and enables the driver in the gki_defconfig
    files.
    
    Bug: 278052745
    Co-developed-by: Wedson Almeida Filho <wedsonaf@google.com>
    Change-Id: Ib85cfe856a4c4990af4d9d7927d2ebe9c96a817c
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -19,6 +19,7 @@
 #include <linux/security.h>
 #include <linux/poll.h>
 #include <linux/workqueue.h>
+#include <uapi/linux/android/binder.h>
 
 /* `bindgen` gets confused at certain things. */
 const gfp_t BINDINGS_GFP_KERNEL = GFP_KERNEL;
