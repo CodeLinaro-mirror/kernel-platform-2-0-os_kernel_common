10288484ebc98fa2dc6687d3cfcd944c01ab9270
Alice Ryhl <aliceryhl@google.com>
2023-09-08 14:15:32 +0200

    FROMLIST: rust: sync: add `CondVar::wait_timeout`
    Sleep on a condition variable with a timeout.
    
    This is used by Rust Binder for process freezing. There, we want to
    sleep until the freeze operation completes, but we want to be able to
    abort the process freezing if it doesn't complete within some timeout.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    
    Bug: 278052745
    Link: https://lore.kernel.org/all/20231206-rb-new-condvar-methods-v1-2-33a4cab7fdaa@google.com/
    Change-Id: I75dc41f5eefbfa8aaacc85cdf02a5f08175bb6e7
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/sync.rs
+++ rust/kernel/sync.rs
@@ -14,7 +14,7 @@ mod locked_by;
 pub mod poll;
 
 pub use arc::{Arc, ArcBorrow, UniqueArc};
-pub use condvar::CondVar;
+pub use condvar::{CondVar, CondVarTimeoutResult};
 pub use lock::{mutex::Mutex, spinlock::SpinLock};
 pub use locked_by::LockedBy;
 
