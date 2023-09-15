aa94b84f837af1f264265b2c9831553a4501fc76
SeongJae Park <sj@kernel.org>
2023-09-15 02:52:44 +0000

    UPSTREAM: mm/damon/core: define and use a dedicated function for region access rate update
    Patch series "mm/damon: provide pseudo-moving sum based access rate".
    
    DAMON checks the access to each region for every sampling interval,
    increase the access rate counter of the region, namely nr_accesses, if the
    access was made.  For every aggregation interval, the counter is reset.
    The counter is exposed to users to be used as a metric showing the
    relative access rate (frequency) of each region.  In other words, DAMON
    provides access rate of each region in every aggregation interval.  The
    aggregation avoids temporal access pattern changes making things
    confusing.  However, this also makes a few DAMON-related operations to
    unnecessarily need to be aligned to the aggregation interval.  This can
    restrict the flexibility of DAMON applications, especially when the
    aggregation interval is huge.
    
    To provide the monitoring results in finer-grained timing while keeping
    handling of temporal access pattern change, this patchset implements a
    pseudo-moving sum based access rate metric.  It is pseudo-moving sum
    because strict moving sum implementation would need to keep all values for
    last time window, and that could incur high overhead of there could be
    arbitrary number of values in a time window.  Especially in case of the
    nr_accesses, since the sampling interval and aggregation interval can
    arbitrarily set and the past values should be maintained for every region,
    it could be risky.  The pseudo-moving sum assumes there were no temporal
    access pattern change in last discrete time window to remove the needs for
    keeping the list of the last time window values.  As a result, it beocmes
    not strict moving sum implementation, but provides a reasonable accuracy.
    
    Also, it keeps an important property of the moving sum.  That is, the
    moving sum becomes same to discrete-window based sum at the time that
    aligns to the time window.  This means using the pseudo moving sum based
    nr_accesses makes no change to users who shows the value for every
    aggregation interval.
    
    Patches Sequence
    ----------------
    
    The sequence of the patches is as follows.  The first four patches are for
    preparation of the change.  The first two (patches 1 and 2) implements a
    helper function for nr_accesses update and eliminate corner case that
    skips use of the function, respectively.  Following two (patches 3 and 4)
    respectively implement the pseudo-moving sum function and its simple unit
    test case.
    
    Two patches for making DAMON to use the pseudo-moving sum follow.  The
    fifthe one (patch 5) introduces a new field for representing the
    pseudo-moving sum-based access rate of each region, and the sixth one
    makes the new representation to actually updated with the pseudo-moving
    sum function.
    
    Last two patches (patches 7 and 8) makes followup fixes for skipping
    unnecessary updates and marking the moving sum function as static,
    respectively.
    
    This patch (of 8):
    
    Each DAMON operarions set is updating nr_accesses field of each
    damon_region for each of their access check results, from the
    check_accesses() callback.  Directly accessing the field could make things
    complex to manage and change in future.  Define and use a dedicated
    function for the purpose.
    
    Link: https://lkml.kernel.org/r/20230915025251.72816-1-sj@kernel.org
    Link: https://lkml.kernel.org/r/20230915025251.72816-2-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Brendan Higgins <brendanhiggins@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 78fbfb155d204428119310d1b9df665ab88da6e8)
    
    Bug: 300502883
    Change-Id: I79ade71a298319174515123e0aaec8aa2f9065d3
    Signed-off-by: cui yangpei <cuiyangpei@xiaomi.com>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -44,7 +44,9 @@ struct damon_addr_range {
  *
  * @nr_accesses is reset to zero for every &damon_attrs->aggr_interval and be
  * increased for every &damon_attrs->sample_interval if an access to the region
- * during the last sampling interval is found.
+ * during the last sampling interval is found.  The update of this field should
+ * not be done with direct access but with the helper function,
+ * damon_update_region_access_rate().
  *
  * @age is initially zero, increased for each aggregation interval, and reset
  * to zero again if the access frequency is significantly changed.  If two
@@ -556,6 +558,7 @@ void damon_add_region(struct damon_region *r, struct damon_target *t);
 void damon_destroy_region(struct damon_region *r, struct damon_target *t);
 int damon_set_regions(struct damon_target *t, struct damon_addr_range *ranges,
 		unsigned int nr_ranges);
+void damon_update_region_access_rate(struct damon_region *r, bool accessed);
 
 struct damos *damon_new_scheme(struct damos_access_pattern *pattern,
 			enum damos_action action, struct damos_quota *quota,
