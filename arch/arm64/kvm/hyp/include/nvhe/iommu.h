96ba8a12687b8789b7fb4de1cbb48a9bb96b6118
Mostafa Saleh <smostafa@google.com>
2023-10-04 13:40:13 +0000

    ANDROID: KVM: arm64: iommu: Alloc domain memory from the pool
    As now we can alloc contiguous memory, no need to pass pgd in alloc
    domain HVC.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: If074047d272e6c330b3bc10e4407ef5d8ed931e9
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -9,7 +9,6 @@
 #include <linux/io-pgtable-arm.h>
 
 struct io_pgtable *kvm_arm_io_pgtable_alloc(struct io_pgtable_cfg *cfg,
-					    unsigned long pgd_hva,
 					    void *cookie,
 					    int *out_ret);
 int kvm_arm_io_pgtable_free(struct io_pgtable *iop);
@@ -26,7 +25,7 @@ void kvm_iommu_reclaim_pages(void *p, u8 order);
 #define kvm_iommu_reclaim_page(p)		kvm_iommu_reclaim_pages(p, 0)
 
 /* Hypercall handlers */
-int kvm_iommu_alloc_domain(pkvm_handle_t domain_id, unsigned long pgd_hva);
+int kvm_iommu_alloc_domain(pkvm_handle_t domain_id);
 int kvm_iommu_free_domain(pkvm_handle_t domain_id);
 int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 			 u32 endpoint_id);
@@ -39,8 +38,7 @@ size_t kvm_iommu_unmap_pages(pkvm_handle_t domain_id,
 			     unsigned long iova, size_t pgsize, size_t pgcount);
 phys_addr_t kvm_iommu_iova_to_phys(pkvm_handle_t domain_id, unsigned long iova);
 #else /* !CONFIG_KVM_IOMMU */
-static inline int kvm_iommu_alloc_domain(pkvm_handle_t domain_id,
-					 unsigned long pgd_hva);
+static inline int kvm_iommu_alloc_domain(pkvm_handle_t domain_id)
 {
 	return -ENODEV;
 }
@@ -94,7 +92,7 @@ static inline int kvm_iommu_init(struct kvm_iommu_ops *ops,
 struct kvm_iommu_ops {
 	int (*init)(unsigned long arg);
 	struct kvm_hyp_iommu *(*get_iommu_by_id)(pkvm_handle_t smmu_id);
-	int (*alloc_domain)(struct kvm_hyp_iommu_domain *domain, unsigned long pgd_hva);
+	int (*alloc_domain)(struct kvm_hyp_iommu_domain *domain);
 	void (*free_domain)(struct kvm_hyp_iommu_domain *domain);
 	int (*attach_dev)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
 			  u32 endpoint_id);
