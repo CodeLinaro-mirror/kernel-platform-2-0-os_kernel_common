9b9c948dbe7b5ffae6dfef13950dd06200da357f
SeongJae Park <sj@kernel.org>
2023-11-30 02:36:46 +0000

    BACKPORT: mm/damon/sysfs-schemes: commit damos quota goals user input to DAMOS
    Make DAMON sysfs interface to read the user inputs for DAMOS quota goals
    and pass those to DAMOS, so that the users can use the quota auto-tuning
    feature.  It uses the DAMON sysfs interface's user input commit mechanism,
    which applies all user inputs for initial starting of DAMON and online
    input updates, which can be done by writing 'on' and 'commit' to the
    kdamond's 'state' file, respectively.  In other words, the user should
    periodically write appropriate value to 'current_value' files and 'commit'
    command to the 'state' file.  'target_value' files could also be similarly
    updated at any time.
    
    Note that the interface is supporting multiple goals while the core logic
    supports only one goal.  DAMON sysfs interface passes only best feedback
    among the given inputs, to avoid making DAMOS too aggressive.
    
    Link: https://lkml.kernel.org/r/20231130023652.50284-4-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Brendan Higgins <brendanhiggins@google.com>
    Cc: David Gow <davidgow@google.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 7f262da0a30df32a960f90ab6c11b08a7233ea34)
    [cuiyangpei: no need scheme filters feature, so did not cherry pick
    29cbb9a13f05b20f0777c60db9603730b487a4e0]
    
    Bug: 300502883
    Change-Id: Idba5d83e4c804789b49c7db249cce28c50c85d18
    Signed-off-by: cui yangpei <cuiyangpei@xiaomi.com>

--- mm/damon/sysfs-schemes.c
+++ mm/damon/sysfs-schemes.c
@@ -1434,6 +1434,34 @@ struct kobj_type damon_sysfs_schemes_ktype = {
 	.default_groups = damon_sysfs_schemes_groups,
 };
 
+static unsigned long damos_sysfs_get_quota_score(void *arg)
+{
+	return (unsigned long)arg;
+}
+
+static void damos_sysfs_set_quota_score(
+		struct damos_sysfs_quota_goals *sysfs_goals,
+		struct damos_quota *quota)
+{
+	struct damos_sysfs_quota_goal *sysfs_goal;
+	int i;
+
+	quota->get_score = NULL;
+	quota->get_score_arg = (void *)0;
+	for (i = 0; i < sysfs_goals->nr; i++) {
+		sysfs_goal = sysfs_goals->goals_arr[i];
+		if (!sysfs_goal->target_value)
+			continue;
+
+		/* Higher score makes scheme less aggressive */
+		quota->get_score_arg = (void *)max(
+				(unsigned long)quota->get_score_arg,
+				sysfs_goal->current_value * 10000 /
+				sysfs_goal->target_value);
+		quota->get_score = damos_sysfs_get_quota_score;
+	}
+}
+
 static struct damos *damon_sysfs_mk_scheme(
 		struct damon_sysfs_scheme *sysfs_scheme)
 {
@@ -1467,6 +1495,8 @@ static struct damos *damon_sysfs_mk_scheme(
 		.low = sysfs_wmarks->low,
 	};
 
+	damos_sysfs_set_quota_score(sysfs_quotas->goals, &quota);
+
 	return damon_new_scheme(&pattern, sysfs_scheme->action,
 			sysfs_scheme->apply_interval_us, &quota, &wmarks);
 }
@@ -1496,6 +1526,8 @@ static void damon_sysfs_update_scheme(struct damos *scheme,
 	scheme->quota.weight_sz = sysfs_weights->sz;
 	scheme->quota.weight_nr_accesses = sysfs_weights->nr_accesses;
 	scheme->quota.weight_age = sysfs_weights->age;
+
+	damos_sysfs_set_quota_score(sysfs_quotas->goals, &scheme->quota);
 
 	scheme->wmarks.metric = sysfs_wmarks->metric;
 	scheme->wmarks.interval = sysfs_wmarks->interval_us;
