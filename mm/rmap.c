31b851e498e4cb62983c86b9e23da880e345ec56
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-01-11 15:24:28 +0000

    UPSTREAM: mm: convert mm_counter() to take a folio
    Now all callers of mm_counter() have a folio, convert mm_counter() to take
    a folio.  Saves a call to compound_head() hidden inside PageAnon().
    
    Link: https://lkml.kernel.org/r/20240111152429.3374566-10-willy@infradead.org
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit a23f517b0e1554467b0eb3bc1ebcb4d626217302)
    
    Bug: 313807618
    Change-Id: I3319518e4199ffd1c3f1c80ef96d277b21cd34dd
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1756,7 +1756,7 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 				set_huge_pte_at(mm, address, pvmw.pte, pteval,
 						hsz);
 			} else {
-				dec_mm_counter(mm, mm_counter(&folio->page));
+				dec_mm_counter(mm, mm_counter(folio));
 				set_pte_at(mm, address, pvmw.pte, pteval);
 			}
 
@@ -1771,7 +1771,7 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 			 * migration) will not expect userfaults on already
 			 * copied pages.
 			 */
-			dec_mm_counter(mm, mm_counter(&folio->page));
+			dec_mm_counter(mm, mm_counter(folio));
 		} else if (folio_test_anon(folio)) {
 			swp_entry_t entry = page_swap_entry(subpage);
 			pte_t swp_pte;
@@ -2158,7 +2158,7 @@ static bool try_to_migrate_one(struct folio *folio, struct vm_area_struct *vma,
 				set_huge_pte_at(mm, address, pvmw.pte, pteval,
 						hsz);
 			} else {
-				dec_mm_counter(mm, mm_counter(&folio->page));
+				dec_mm_counter(mm, mm_counter(folio));
 				set_pte_at(mm, address, pvmw.pte, pteval);
 			}
 
@@ -2173,7 +2173,7 @@ static bool try_to_migrate_one(struct folio *folio, struct vm_area_struct *vma,
 			 * migration) will not expect userfaults on already
 			 * copied pages.
 			 */
-			dec_mm_counter(mm, mm_counter(&folio->page));
+			dec_mm_counter(mm, mm_counter(folio));
 		} else {
 			swp_entry_t entry;
 			pte_t swp_pte;
