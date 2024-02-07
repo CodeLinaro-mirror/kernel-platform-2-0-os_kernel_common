7bfa9456f1666ad27a000865c99cf9f83037ecd1
Kalesh Singh <kaleshsingh@google.com>
2024-02-06 22:33:30 -0800

    ANDROID: 16K: Reduce mmap rand bits
    The emulated page size it userspace is larger than the kernel page size.
    This means that the kernel can no longer porvide the max amount of mmap
    rand bits as before. The maximum must be reduced by the difference
    between the emulated page shift and the real page shift.
    
    Also move initialization of mmap_rnd_bits_* globals to
    init_mmap_rnd_bits(), which is called at core_init time. This is
    necessary since the values are no longer build time constants.
    
    Bug: 315325080
    Bug: 302403436
    Change-Id: I40e47ecbff2d2c7d0189ab2c74e2b34f5367f47e
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/page_size_compat.c
+++ mm/page_size_compat.c
@@ -11,6 +11,7 @@
 #include <linux/init.h>
 #include <linux/jump_label.h>
 #include <linux/kstrtox.h>
+#include <linux/mm.h>
 
 #if defined(CONFIG_PAGE_SHIFT_COMPAT) && CONFIG_PAGE_SHIFT_COMPAT > PAGE_SHIFT
 DEFINE_STATIC_KEY_FALSE(page_size_compat);
@@ -39,4 +40,23 @@ static int __init early_page_size_compat(char *buf)
 	return 0;
 }
 early_param("page_size_compat", early_page_size_compat);
+
+#define __MMAP_RND_BITS(x)      (x - (__PAGE_SHIFT - PAGE_SHIFT))
+
+static int __init init_mmap_rnd_bits(void)
+{
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_BITS
+	mmap_rnd_bits_min = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_BITS_MIN);
+	mmap_rnd_bits_max = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_BITS_MAX);
+	mmap_rnd_bits = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_BITS);
+#endif
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_COMPAT_BITS
+	mmap_rnd_compat_bits_min = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MIN);
+	mmap_rnd_compat_bits_max = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MAX);
+	mmap_rnd_compat_bits = __MMAP_RND_BITS(CONFIG_ARCH_MMAP_RND_COMPAT_BITS);
+#endif
+	return 0;
+}
+core_initcall(init_mmap_rnd_bits);
+
 #endif /* defined(CONFIG_PAGE_SHIFT_COMPAT) && CONFIG_PAGE_SHIFT_COMPAT > PAGE_SHIFT */
