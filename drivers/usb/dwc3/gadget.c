bb709d96edd73d00a5b32c6104f758a09975350a
Ke Du <quic_kedu@quicinc.com>
2024-03-05 18:09:17 +0800

    Subject: [PATCH] UPSTREAM: usb: dwc3: gadget: Delay issuing End Transfer
    If the controller hasn't DMA'ed the Setup data from its fifo, it won't
    process the End Transfer command. Polling for the command completion may
    block the driver from servicing the Setup phase and cause a timeout.
    Previously we only check and delay issuing End Transfer in the case of
    endpoint dequeue. Let's do that for all End Transfer scenarios.
    
    Signed-off-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Link: https://lore.kernel.org/r/2fcf3b5d90068d549589a57a27a79f76c6769b04.1650593829.git.Thinh.Nguyen@synopsys.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit f66eef8fb8989a7193cafc3870f7c7b2b97f16cb)
    
    Bug: 263189538
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>
    Signed-off-by: Ke Du <quic_kedu@quicinc.com>
    (cherry picked from commit 682dee1783127d0d4f07ca22340d97e4caf99b9d)
    
    Change-Id: I5d8f7fd44d45393b8631f73da01f483d29559dcb
    Signed-off-by: Guang <quic_guyang@quicinc.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2072,16 +2072,6 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 		if (r == req) {
 			struct dwc3_request *t;
 
-			/*
-			 * If a Setup packet is received but yet to DMA out, the controller will
-			 * not process the End Transfer command of any endpoint. Polling of its
-			 * DEPCMD.CmdAct may block setting up TRB for Setup packet, causing a
-			 * timeout. Delay issuing the End Transfer command until the Setup TRB is
-			 * prepared.
-			 */
-			if (dwc->ep0state != EP0_SETUP_PHASE && !dwc->delayed_status)
-				dep->flags |= DWC3_EP_DELAY_STOP;
-
 			/* wait until it is processed */
 			dwc3_stop_active_transfer(dep, true, true);
 
