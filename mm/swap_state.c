3d4aad891de7bbcbeed6f94c8f5f81be2caf3b5f
Kairui Song <kasong@tencent.com>
2024-05-22 01:58:53 +0800

    BACKPORT: mm/swap: reduce swap cache search space
    Currently we use one swap_address_space for every 64M chunk to reduce lock
    contention, this is like having a set of smaller swap files inside one
    swap device.  But when doing swap cache look up or insert, we are still
    using the offset of the whole large swap device.  This is OK for
    correctness, as the offset (key) is unique.
    
    But Xarray is specially optimized for small indexes, it creates the radix
    tree levels lazily to be just enough to fit the largest key stored in one
    Xarray.  So we are wasting tree nodes unnecessarily.
    
    For 64M chunk it should only take at most 3 levels to contain everything.
    But if we are using the offset from the whole swap device, the offset
    (key) value will be way beyond 64M, and so will the tree level.
    
    Optimize this by using a new helper swap_cache_index to get a swap entry's
    unique offset in its own 64M swap_address_space.
    
    I see a ~1% performance gain in benchmark and actual workload with high
    memory pressure.
    
    Test with `time memhog 128G` inside a 8G memcg using 128G swap (ramdisk
    with SWP_SYNCHRONOUS_IO dropped, tested 3 times, results are stable.  The
    test result is similar but the improvement is smaller if
    SWP_SYNCHRONOUS_IO is enabled, as swap out path can never skip swap
    cache):
    
    Before:
    6.07user 250.74system 4:17.26elapsed 99%CPU (0avgtext+0avgdata 8373376maxresident)k
    0inputs+0outputs (55major+33555018minor)pagefaults 0swaps
    
    After (1.8% faster):
    6.08user 246.09system 4:12.58elapsed 99%CPU (0avgtext+0avgdata 8373248maxresident)k
    0inputs+0outputs (54major+33555027minor)pagefaults 0swaps
    
    Similar result with MySQL and sysbench using swap:
    Before:
    94055.61 qps
    
    After (0.8% faster):
    94834.91 qps
    
    Radix tree slab usage is also very slightly lower.
    
    Link: https://lkml.kernel.org/r/20240521175854.96038-12-ryncsn@gmail.com
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Cc: Anna Schumaker <anna@kernel.org>
    Cc: Barry Song <v-songbaohua@oppo.com>
    Cc: Chao Yu <chao@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: Jaegeuk Kim <jaegeuk@kernel.org>
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Marc Dionne <marc.dionne@auristor.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: NeilBrown <neilb@suse.de>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Ryusuke Konishi <konishi.ryusuke@gmail.com>
    Cc: Trond Myklebust <trond.myklebust@hammerspace.com>
    Cc: Xiubo Li <xiubli@redhat.com>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 7aad25b4b47ea5b67e1eb8be0db211b899dce60d)
    [surenb: merge conflict due to __folio_swap_cache_index() being called
    __page_file_index() in 6.6]
    Bug: 313807618
    Bug: 351082780
    Change-Id: Idbfbf1f8eddecea486059a81f5e092763bff1b71
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -70,7 +70,7 @@ void show_swap_cache_info(void)
 void *get_shadow_from_swap_cache(swp_entry_t entry)
 {
 	struct address_space *address_space = swap_address_space(entry);
-	pgoff_t idx = swp_offset(entry);
+	pgoff_t idx = swap_cache_index(entry);
 	struct page *page;
 
 	page = xa_load(&address_space->i_pages, idx);
@@ -87,7 +87,7 @@ int add_to_swap_cache(struct folio *folio, swp_entry_t entry,
 			gfp_t gfp, void **shadowp)
 {
 	struct address_space *address_space = swap_address_space(entry);
-	pgoff_t idx = swp_offset(entry);
+	pgoff_t idx = swap_cache_index(entry);
 	XA_STATE_ORDER(xas, &address_space->i_pages, idx, folio_order(folio));
 	unsigned long i, nr = folio_nr_pages(folio);
 	void *old;
@@ -142,7 +142,7 @@ void __delete_from_swap_cache(struct folio *folio,
 	struct address_space *address_space = swap_address_space(entry);
 	int i;
 	long nr = folio_nr_pages(folio);
-	pgoff_t idx = swp_offset(entry);
+	pgoff_t idx = swap_cache_index(entry);
 	XA_STATE(xas, &address_space->i_pages, idx);
 
 	xas_set_update(&xas, workingset_update_node);
@@ -251,13 +251,14 @@ void clear_shadow_from_swap_cache(int type, unsigned long begin,
 
 	for (;;) {
 		swp_entry_t entry = swp_entry(type, curr);
+		unsigned long index = curr & SWAP_ADDRESS_SPACE_MASK;
 		struct address_space *address_space = swap_address_space(entry);
-		XA_STATE(xas, &address_space->i_pages, curr);
+		XA_STATE(xas, &address_space->i_pages, index);
 
 		xas_set_update(&xas, workingset_update_node);
 
 		xa_lock_irq(&address_space->i_pages);
-		xas_for_each(&xas, old, end) {
+		xas_for_each(&xas, old, min(index + (end - curr), SWAP_ADDRESS_SPACE_PAGES)) {
 			if (!xa_is_value(old))
 				continue;
 			xas_store(&xas, NULL);
@@ -344,7 +345,7 @@ struct folio *swap_cache_get_folio(swp_entry_t entry,
 {
 	struct folio *folio;
 
-	folio = filemap_get_folio(swap_address_space(entry), swp_offset(entry));
+	folio = filemap_get_folio(swap_address_space(entry), swap_cache_index(entry));
 	if (!IS_ERR(folio)) {
 		bool vma_ra = swap_use_vma_readahead();
 		bool readahead;
@@ -414,7 +415,7 @@ struct folio *filemap_get_incore_folio(struct address_space *mapping,
 	si = get_swap_device(swp);
 	if (!si)
 		return ERR_PTR(-ENOENT);
-	index = swp_offset(swp);
+	index = swap_cache_index(swp);
 	folio = filemap_get_folio(swap_address_space(swp), index);
 	put_swap_device(si);
 	return folio;
@@ -442,7 +443,7 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 		 * that would confuse statistics.
 		 */
 		folio = filemap_get_folio(swap_address_space(entry),
-						swp_offset(entry));
+					  swap_cache_index(entry));
 		if (!IS_ERR(folio)) {
 			page = folio_file_page(folio, swp_offset(entry));
 			goto got_page;
