ca767af0b9dc8822610febd4551764524cb54f06
Kairui Song <kasong@tencent.com>
2024-05-22 01:58:53 +0800

    BACKPORT: mm/swap: reduce swap cache search space
    Currently we use one swap_address_space for every 64M chunk to reduce lock
    contention, this is like having a set of smaller swap files inside one
    swap device.  But when doing swap cache look up or insert, we are still
    using the offset of the whole large swap device.  This is OK for
    correctness, as the offset (key) is unique.
    
    But Xarray is specially optimized for small indexes, it creates the radix
    tree levels lazily to be just enough to fit the largest key stored in one
    Xarray.  So we are wasting tree nodes unnecessarily.
    
    For 64M chunk it should only take at most 3 levels to contain everything.
    But if we are using the offset from the whole swap device, the offset
    (key) value will be way beyond 64M, and so will the tree level.
    
    Optimize this by using a new helper swap_cache_index to get a swap entry's
    unique offset in its own 64M swap_address_space.
    
    I see a ~1% performance gain in benchmark and actual workload with high
    memory pressure.
    
    Test with `time memhog 128G` inside a 8G memcg using 128G swap (ramdisk
    with SWP_SYNCHRONOUS_IO dropped, tested 3 times, results are stable.  The
    test result is similar but the improvement is smaller if
    SWP_SYNCHRONOUS_IO is enabled, as swap out path can never skip swap
    cache):
    
    Before:
    6.07user 250.74system 4:17.26elapsed 99%CPU (0avgtext+0avgdata 8373376maxresident)k
    0inputs+0outputs (55major+33555018minor)pagefaults 0swaps
    
    After (1.8% faster):
    6.08user 246.09system 4:12.58elapsed 99%CPU (0avgtext+0avgdata 8373248maxresident)k
    0inputs+0outputs (54major+33555027minor)pagefaults 0swaps
    
    Similar result with MySQL and sysbench using swap:
    Before:
    94055.61 qps
    
    After (0.8% faster):
    94834.91 qps
    
    Radix tree slab usage is also very slightly lower.
    
    Link: https://lkml.kernel.org/r/20240521175854.96038-12-ryncsn@gmail.com
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Cc: Anna Schumaker <anna@kernel.org>
    Cc: Barry Song <v-songbaohua@oppo.com>
    Cc: Chao Yu <chao@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: Jaegeuk Kim <jaegeuk@kernel.org>
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Marc Dionne <marc.dionne@auristor.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: NeilBrown <neilb@suse.de>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Ryusuke Konishi <konishi.ryusuke@gmail.com>
    Cc: Trond Myklebust <trond.myklebust@hammerspace.com>
    Cc: Xiubo Li <xiubli@redhat.com>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 7aad25b4b47ea5b67e1eb8be0db211b899dce60d)
    [surenb: merge conflict due to __folio_swap_cache_index() being called
    __page_file_index() in 6.6]
    Bug: 313807618
    Bug: 351082780
    Change-Id: Idbfbf1f8eddecea486059a81f5e092763bff1b71
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -148,7 +148,7 @@ static int __try_to_reclaim_swap(struct swap_info_struct *si,
 	struct folio *folio;
 	int ret = 0;
 
-	folio = filemap_get_folio(swap_address_space(entry), offset);
+	folio = filemap_get_folio(swap_address_space(entry), swap_cache_index(entry));
 	if (IS_ERR(folio))
 		return 0;
 	/*
@@ -2136,7 +2136,7 @@ retry:
 	       (i = find_next_to_unuse(si, i)) != 0) {
 
 		entry = swp_entry(type, i);
-		folio = filemap_get_folio(swap_address_space(entry), i);
+		folio = filemap_get_folio(swap_address_space(entry), swap_cache_index(entry));
 		if (IS_ERR(folio))
 			continue;
 
@@ -3481,7 +3481,7 @@ EXPORT_SYMBOL_GPL(swapcache_mapping);
 pgoff_t __page_file_index(struct page *page)
 {
 	swp_entry_t swap = page_swap_entry(page);
-	return swp_offset(swap);
+	return swap_cache_index(swap);
 }
 EXPORT_SYMBOL_GPL(__page_file_index);
 
