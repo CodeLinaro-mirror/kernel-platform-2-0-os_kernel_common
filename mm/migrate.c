4ac7f73acaf221e1cbbe15eb85f305dcc6ef3398
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-05-24 13:28:43 +0800

    mm: remove MIGRATE_SYNC_NO_COPY mode
    Commit 2916ecc0f9d4 ("mm/migrate: new migrate mode MIGRATE_SYNC_NO_COPY")
    introduce a new MIGRATE_SYNC_NO_COPY mode to allow to offload the copy to
    a device DMA engine, which is only used __migrate_device_pages() to decide
    whether or not copy the old page, and the MIGRATE_SYNC_NO_COPY mode only
    set in hmm, as the MIGRATE_SYNC_NO_COPY set is removed by previous
    cleanup, it seems that we could remove the unnecessary
    MIGRATE_SYNC_NO_COPY.
    
    Link: https://lkml.kernel.org/r/20240524052843.182275-6-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Jane Chu <jane.chu@oracle.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Benjamin LaHaise <bcrl@kvack.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: Jiaqi Yan <jiaqiyan@google.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Naoya Horiguchi <nao.horiguchi@gmail.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Change-Id: Ia1641e0786d8480d09b959b5b28fbac59ea5167b

--- mm/migrate.c
+++ mm/migrate.c
@@ -684,10 +684,7 @@ static int __migrate_folio(struct address_space *mapping, struct folio *dst,
 	if (src_private)
 		folio_attach_private(dst, folio_detach_private(src));
 
-	if (mode != MIGRATE_SYNC_NO_COPY)
-		folio_migrate_copy(dst, src);
-	else
-		folio_migrate_flags(dst, src);
+	folio_migrate_copy(dst, src);
 	return MIGRATEPAGE_SUCCESS;
 }
 
@@ -916,7 +913,6 @@ static int fallback_migrate_folio(struct address_space *mapping,
 		/* Only writeback folios in full synchronous migration */
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			return -EBUSY;
@@ -1174,7 +1170,6 @@ static int migrate_folio_unmap(new_folio_t get_new_folio,
 		 */
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			rc = -EBUSY;
@@ -1385,7 +1380,6 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 			goto out;
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			goto out;
