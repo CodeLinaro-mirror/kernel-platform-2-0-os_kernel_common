9c6fed5d12cf26f27de5958e1635f2039d9d6fbc
Kalesh Singh <kaleshsingh@google.com>
2024-06-04 20:52:10 -0700

    Revert "UPSTREAM: arm64/mm: new ptep layer to manage contig bit"
    This reverts commit 39b7225eab81d594c07aeee23f23b9a26ccaf25a.
    
    Change-Id: I0f24f6e1acbd8c01e80b744f1e145e6007d286e4
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- arch/arm64/mm/fixmap.c
+++ arch/arm64/mm/fixmap.c
@@ -121,9 +121,9 @@ void __set_fixmap(enum fixed_addresses idx,
 	ptep = fixmap_pte(addr);
 
 	if (pgprot_val(flags)) {
-		__set_pte(ptep, pfn_pte(phys >> PAGE_SHIFT, flags));
+		set_pte(ptep, pfn_pte(phys >> PAGE_SHIFT, flags));
 	} else {
-		__pte_clear(&init_mm, addr, ptep);
+		pte_clear(&init_mm, addr, ptep);
 		flush_tlb_kernel_range(addr, addr+PAGE_SIZE);
 	}
 }
