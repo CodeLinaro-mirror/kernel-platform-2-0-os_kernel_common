f25c697e91f780bd1cd738c679b40df1ccb8c13a
Alice Ryhl <aliceryhl@google.com>
2024-06-07 16:42:58 +0000

    ANDROID: rust_binder: directly access transaction fields from C
    Provide a way for the Rust binder to publish information about the
    layout of the Transaction struct so that C code can directly read those
    fields. This allows the rust_binder_transaction tracepoint to read those
    fields inside TP_fast_assign.
    
    The intent is that vendorhooks will access information in the same
    manner. As long as the vendorhook relies on the information in
    RUST_BINDER_LAYOUT at runtime, changes to the layout of the Rust types
    will not break vendorhooks.
    
    Bug: 348589263
    Change-Id: I8b346993a98bd9dba3b48f3f4e17f0790496772c
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/trace.rs
+++ drivers/android/binder/trace.rs
@@ -2,16 +2,22 @@
 
 // Copyright (C) 2024 Google LLC.
 
+use crate::transaction::Transaction;
+
+use kernel::bindings::rust_binder_transaction;
 use kernel::tracepoint::declare_trace;
 
-use core::ffi::c_int;
-
 declare_trace! {
-    fn rust_binder_transaction(debug_id: c_int, reply: bool);
+    fn rust_binder_transaction(reply: bool, t: rust_binder_transaction);
 }
 
 #[inline]
-pub(crate) fn trace_transaction(debug_id: usize, reply: bool) {
-    // SAFETY: No safety requirements for this tracepoint.
-    unsafe { rust_binder_transaction(debug_id as c_int, reply) }
+fn raw_transaction(t: &Transaction) -> rust_binder_transaction {
+    t as *const Transaction as rust_binder_transaction
+}
+
+#[inline]
+pub(crate) fn trace_transaction(reply: bool, t: &Transaction) {
+    // SAFETY: The raw transaction is valid for the duration of this call.
+    unsafe { rust_binder_transaction(reply, raw_transaction(t)) }
 }
