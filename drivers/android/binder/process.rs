b43255ffce80e3685827a48e5edef9992a272b5d
Alice Ryhl <aliceryhl@google.com>
2024-06-12 13:32:31 +0000

    ANDROID: rust_binder: add tracepoints for exiting from functions
    Also adjusts the error handling slightly to make it more convenient to
    introduce the tracepoints.
    
    Bug: 348589263
    Change-Id: I86dd41d03668356207df787fb21ca4183f83f416
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/process.rs
+++ drivers/android/binder/process.rs
@@ -1396,7 +1396,7 @@ impl Process {
         _file: &File,
         cmd: u32,
         reader: &mut UserSliceReader,
-    ) -> Result<i32> {
+    ) -> Result {
         let thread = this.get_current_thread()?;
         match cmd {
             bindings::BINDER_SET_MAX_THREADS => this.set_max_threads(reader.read()?),
@@ -1411,7 +1411,7 @@ impl Process {
             bindings::BINDER_FREEZE => ioctl_freeze(reader)?,
             _ => return Err(EINVAL),
         }
-        Ok(0)
+        Ok(())
     }
 
     /// Ioctls that are read/write from the perspective of userspace.
@@ -1422,7 +1422,7 @@ impl Process {
         file: &File,
         cmd: u32,
         data: UserSlice,
-    ) -> Result<i32> {
+    ) -> Result {
         let thread = this.get_current_thread()?;
         let blocking = (file.flags() & file::flags::O_NONBLOCK) == 0;
         match cmd {
@@ -1434,7 +1434,7 @@ impl Process {
             bindings::BINDER_GET_EXTENDED_ERROR => thread.get_extended_error(data)?,
             _ => return Err(EINVAL),
         }
-        Ok(0)
+        Ok(())
     }
 }
 
@@ -1480,7 +1480,7 @@ impl Process {
         file: &File,
         cmd: u32,
         arg: *mut core::ffi::c_void,
-    ) -> Result<i32> {
+    ) -> Result {
         use kernel::ioctl::{_IOC_DIR, _IOC_SIZE};
         use kernel::uapi::{_IOC_READ, _IOC_WRITE};
 
@@ -1490,11 +1490,14 @@ impl Process {
 
         const _IOC_READ_WRITE: u32 = _IOC_READ | _IOC_WRITE;
 
-        match _IOC_DIR(cmd) {
+        let res = match _IOC_DIR(cmd) {
             _IOC_WRITE => Self::ioctl_write_only(this, file, cmd, &mut user_slice.reader()),
             _IOC_READ_WRITE => Self::ioctl_write_read(this, file, cmd, user_slice),
             _ => Err(EINVAL),
-        }
+        };
+
+        crate::trace::trace_ioctl_done(res);
+        res
     }
 
     pub(crate) fn compat_ioctl(
@@ -1502,7 +1505,7 @@ impl Process {
         file: &File,
         cmd: u32,
         arg: *mut core::ffi::c_void,
-    ) -> Result<i32> {
+    ) -> Result {
         Self::ioctl(this, file, cmd, arg)
     }
 
