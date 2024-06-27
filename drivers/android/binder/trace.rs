570a6e3a3be3135afdc96ab72dbbd4278d4a306d
Alice Ryhl <aliceryhl@google.com>
2024-06-27 14:27:02 +0000

    ANDROID: rust_binder: add trace_rust_binder_transaction_fd_{send,recv}
    This tracepoint is useful for debugging how file descriptors are sent
    from one process to another.
    
    Like with trace_rust_binder_transaction_node_send, this tracepoint takes
    the transaction debug id as an integer argument rather than a pointer to
    the transaction because the transaction object does not yet exist when
    trace_rust_binder_transaction_fd_send runs.
    
    Bug: 348589263
    Change-Id: Id1b60e438df5d1d92fa9f4019eff963390ed6f2b
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/trace.rs
+++ drivers/android/binder/trace.rs
@@ -26,6 +26,8 @@ declare_trace! {
     fn rust_binder_transaction_node_send(t_debug_id: c_int, n: rust_binder_node,
                                          orig: *const flat_binder_object,
                                          trans: *const flat_binder_object);
+    fn rust_binder_transaction_fd_send(t_debug_id: c_int, fd: c_int, offset: usize);
+    fn rust_binder_transaction_fd_recv(t_debug_id: c_int, fd: c_int, offset: usize);
 
     fn android_vh_rust_binder_set_priority(t: rust_binder_transaction, task: *mut task_struct);
     fn android_vh_rust_binder_restore_priority(task: *mut task_struct);
@@ -129,4 +131,16 @@ pub(crate) fn trace_transaction_node_send(
 ) {
     // SAFETY: The pointers are valid for the duration of this call.
     unsafe { rust_binder_transaction_node_send(t_debug_id as c_int, raw_node(n), orig, trans) }
+}
+
+#[inline]
+pub(crate) fn trace_transaction_fd_send(t_debug_id: usize, fd: u32, offset: usize) {
+    // SAFETY: This function is always safe to call.
+    unsafe { rust_binder_transaction_fd_send(t_debug_id as c_int, fd as c_int, offset) }
+}
+
+#[inline]
+pub(crate) fn trace_transaction_fd_recv(t_debug_id: usize, fd: u32, offset: usize) {
+    // SAFETY: This function is always safe to call.
+    unsafe { rust_binder_transaction_fd_recv(t_debug_id as c_int, fd as c_int, offset) }
 }
