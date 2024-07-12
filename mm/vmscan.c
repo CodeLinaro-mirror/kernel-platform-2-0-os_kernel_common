2633cf1ba6f2b2b15688516b5066d1dffaa7d091
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-12 15:30:39 +0900

    ANDROID: vmscan: add vendor hook for shrink_slab_memcg
    Add vendor hook for shrink_slab_memcg.
    This will be used to clear bit of shrinker with SHRINKER_NONSLAB flag on
    shrinker_info map.
    
    Bug: 351175506
    Change-Id: I1159163c4a4620ccb86030e41b210f0c86f5cca7
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -973,8 +973,10 @@ static unsigned long shrink_slab_memcg(gfp_t gfp_mask, int nid,
 
 		/* Call non-slab shrinkers even though kmem is disabled */
 		if (!memcg_kmem_online() &&
-		    !(shrinker->flags & SHRINKER_NONSLAB))
+		    !(shrinker->flags & SHRINKER_NONSLAB)) {
+			trace_android_vh_shrink_slab_memcg_nonslab(i, info);
 			continue;
+		}
 
 		ret = do_shrink_slab(&sc, shrinker, priority);
 		if (ret == SHRINK_EMPTY) {
