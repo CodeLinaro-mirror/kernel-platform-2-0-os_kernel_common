e1da6a9f74eeae7f2b5aba4ede3548ebb86eb40d
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-15 18:32:21 +0900

    ANDROID: mm: add vendor hook to skip split large folio
    Add vendor hook to skip split large folio
    
    Bug: 351175506
    Change-Id: Ibf8303215bfe23ee8d7f3c91aceffcb218c9f9ff
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -321,6 +321,9 @@ DECLARE_HOOK(android_vh_free_pages_ok_bypass,
 	TP_PROTO(struct page *page, unsigned int order,
 		int __bitwise flags, bool *bypass),
 	TP_ARGS(page, order, flags, bypass));
+DECLARE_HOOK(android_vh_split_large_folio_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
 
 DECLARE_HOOK(android_vh_compact_finished,
 	TP_PROTO(bool *abort_compact),
