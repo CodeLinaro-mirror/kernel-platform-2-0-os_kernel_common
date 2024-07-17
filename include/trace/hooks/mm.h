31f4c87c82f6b932906b38defa99ed2aa31ed17f
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-17 10:55:43 +0900

    ANDROID: page_alloc: add vendor hook to bypass rest of __free_pages_ok
    Add a vendor hook to let vendor module to take pages on certain
    conditions to use later instead of freeing pages directly.
    
    Bug: 351175506
    Change-Id: I067ebdd2721dcc1fe95e9ed40d1bbeddf64ac327
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -317,6 +317,10 @@ DECLARE_HOOK(android_vh_free_pages_prepare_bypass,
 	TP_PROTO(struct page *page, unsigned int order,
 		int __bitwise flags, bool *bypass),
 	TP_ARGS(page, order, flags, bypass));
+DECLARE_HOOK(android_vh_free_pages_ok_bypass,
+	TP_PROTO(struct page *page, unsigned int order,
+		int __bitwise flags, bool *bypass),
+	TP_ARGS(page, order, flags, bypass));
 
 DECLARE_HOOK(android_vh_compact_finished,
 	TP_PROTO(bool *abort_compact),
