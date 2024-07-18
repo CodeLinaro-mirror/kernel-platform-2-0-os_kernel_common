0e33b12567fad13686ae6d9d8b6b7d7382536419
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-18 10:17:09 +0900

    ANDROID: page_alloc: add vendor hook to bypass free_pages_prepare
    Add a vendor hook to bypass free_pages_prepare for those pages that have
    never been used by user after free_pages_prepare. This type of pages can
    be created by vendor module implementations.
    
    Bug: 351175506
    Change-Id: I5bebc47fe1be410c78924dd5392260ad0dadaa22
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -313,6 +313,10 @@ DECLARE_HOOK(android_vh_warn_alloc_tune_ratelimit,
 DECLARE_HOOK(android_vh_warn_alloc_show_mem_bypass,
 	TP_PROTO(bool *bypass),
 	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_free_pages_prepare_bypass,
+	TP_PROTO(struct page *page, unsigned int order,
+		int __bitwise flags, bool *skip_free_pages_prepare),
+	TP_ARGS(page, order, flags, skip_free_pages_prepare));
 
 DECLARE_HOOK(android_vh_compact_finished,
 	TP_PROTO(bool *abort_compact),
