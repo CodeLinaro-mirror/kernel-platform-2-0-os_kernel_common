4062a7956ef8079fdf827fe24b888b1b88bad9cf
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-18 10:17:09 +0900

    ANDROID: page_alloc: add vendor hook to bypass free_pages_prepare
    Add a vendor hook to bypass free_pages_prepare for those pages that have
    never been used by user after free_pages_prepare. This type of pages can
    be created by vendor module implementations.
    
    Bug: 351175506
    Change-Id: I5bebc47fe1be410c78924dd5392260ad0dadaa22
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1357,10 +1357,15 @@ static void __free_pages_ok(struct page *page, unsigned int order,
 	unsigned long pfn = page_to_pfn(page);
 	struct zone *zone = page_zone(page);
 	bool skip_free_unref_page = false;
+	bool bypass = false;
+
+	trace_android_vh_free_pages_prepare_bypass(page, order, fpi_flags, &bypass);
+	if (bypass)
+		goto skip_prepare;
 
 	if (!free_pages_prepare(page, order, fpi_flags))
 		return;
-
+skip_prepare:
 	/*
 	 * Calling get_pfnblock_migratetype() without spin_lock_irqsave() here
 	 * is used to avoid calling get_pfnblock_migratetype() under the lock.
