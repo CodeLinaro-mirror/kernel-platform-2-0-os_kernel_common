71e703ed6ec33b0779ec64c23eba2c63c2bfd5a7
Sebastian Ene <sebastianene@google.com>
2024-08-16 11:06:09 +0000

    ANDROID: KVM: arm64: Use the vm handle as the FF-A ID of the guest
    Handle the get id FF-A guest call in the hypervisor and return the id to
    the caller. Pass the vm handle in the w7 to let Trusty identify the caller VM.
    
    Bug: 269285339
    Change-Id: I107b1687dca916e0dc80ca33519b430c37f71cf3
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -36,6 +36,7 @@
 #include <nvhe/memory.h>
 #include <nvhe/trap_handler.h>
 #include <nvhe/spinlock.h>
+#include <kvm/arm_hypercalls.h>
 
 /*
  * "ID value 0 must be returned at the Non-secure physical FF-A instance"
@@ -994,6 +995,8 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 
 	WARN_ON(vm_handle > KVM_MAX_PVMS);
 
+	smccc_set_client_id(vcpu, vm_handle);
+
 	switch (func_id) {
 	case FFA_FEATURES:
 		if (!do_ffa_features(&res, ctxt)) {
@@ -1018,6 +1021,9 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 		break;
 	case FFA_MEM_RECLAIM:
 		do_ffa_mem_reclaim(&res, ctxt, vm_handle);
+		break;
+	case FFA_ID_GET:
+		ffa_to_smccc_res_prop(&res, FFA_RET_SUCCESS, vm_handle);
 		break;
 	default:
 		if (ffa_call_supported(func_id)) {
